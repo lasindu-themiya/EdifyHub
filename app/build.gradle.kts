6b71f574ab6f11662a91ffb1e7a220c4b7ed2793
Anuththara H L L T <lasinduthemiya96@gmail.com>
2025-06-25 20:39:49 +0530

    dependency update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation(libs.mpandroidchart)
 }
