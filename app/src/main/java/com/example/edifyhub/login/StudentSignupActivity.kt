e35fdccb2f176d74309c06ac5cb0d2558e98aee4
Harindu De Silva <harindudesilva12@gmail.com>
2025-06-25 15:07:36 +0530

    navigate through signin and signup pages

--- app/src/main/java/com/example/edifyhub/login/StudentSignupActivity.kt
+++ app/src/main/java/com/example/edifyhub/login/StudentSignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.edifyhub.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.edifyhub.R
 import com.google.android.material.button.MaterialButton
@@ -11,6 +13,15 @@ class StudentSignupActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_student_signup)
+
+        //navigate to signin page
+        val SignInButton = findViewById<TextView>(R.id.signInLink)
+        SignInButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
         val SignupButton = findViewById<MaterialButton>(R.id.signupbtn)
         SignupButton.setOnClickListener {
--- app/src/main/java/com/example/edifyhub/login/TeacherSignupActivity.kt
+++ app/src/main/java/com/example/edifyhub/login/TeacherSignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.edifyhub.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import com.example.edifyhub.R
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
@@ -11,6 +13,15 @@ class TeacherSignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_teacher_signup)
+
+        //navigate to signin page
+        val SignInButton = findViewById<TextView>(R.id.signInLink)
+        SignInButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
 
         val SignupButton = findViewById<MaterialButton>(R.id.teachersignupbtn)
