a95d750da8efdb50b02c5b86e65136851172e59e
Anuththara H L L T <lasinduthemiya96@gmail.com>
2025-06-26 13:16:19 +0530

    conflict fix

--- app/src/main/java/com/example/edifyhub/MainActivity.kt
+++ app/src/main/java/com/example/edifyhub/MainActivity.kt
@@ -1,24 +1,20 @@
 package com.example.edifyhub
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.edifyhub.login.LoginActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        // 1. Create an Intent to start LoginActivity.
-        val intent = Intent(this, LoginActivity::class.java)
-
-        startActivity( intent)
-
-        // 3. Finish MainActivity so the user cannot navigate back to it.
-        finish()
-
-
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
