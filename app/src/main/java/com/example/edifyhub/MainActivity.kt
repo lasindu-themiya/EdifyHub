3dd58d22b16d3d84d7f5805465d252134d0adb71
Anuththara H L L T <lasinduthemiya96@gmail.com>
2025-06-26 12:20:38 +0530

    MainActivity Conflict fix

--- app/src/main/java/com/example/edifyhub/MainActivity.kt
+++ app/src/main/java/com/example/edifyhub/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.edifyhub
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.edifyhub.teacher.TeacherProfileActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Immediately start TeacherProfileActivity
-        val intent = Intent(this, TeacherProfileActivity::class.java)
-        startActivity(intent)
-
-        // Optionally close this activity if you don't want it in back stack
-        finish()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
